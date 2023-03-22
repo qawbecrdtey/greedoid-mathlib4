3db9a37075a9a057b5566296756a11cee41c2855
qawbecrdtey <qawbecrdtey@kaist.ac.kr>
2023-03-22 23:44:47 +0900

    Working on Data.Combinatorics.Greedoid.Basic

--- Mathlib/Combinatorics/Greedoid/Basic.lean
+++ Mathlib/Combinatorics/Greedoid/Basic.lean
@@ -2,53 +2,23 @@ import Mathlib.Data.List.Basic
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Finset.Card
 import Mathlib.Data.Fintype.Basic
+import Mathlib.Data.Fintype.List
 
-/-- A `Word` is an alternative of `List` which gets finite type as an input.
-    Always keep in mind that unlike `List`, left is back and right is front.
-    For example, `w₁` is a prefix of `w₂ ++ w₁`, and w₂ is a postfix. -/
-def Word (α : Type _) [Fintype α] := List α
-
-section Word
-
-variable {α : Type _} [Fintype α]
-
-/-- Length of a word. -/
-protected def Word.length (w : Word α) := List.length w
-
-/-- `w` contains no duplicates. -/
-protected def Word.simple (w : Word α) := w.Nodup
-
-/-- Conversion from `Word α` to `Finset α`. -/
-protected def Word.toFinset [DecidableEq α] (w : Word α) := List.toFinset w
-
-instance : HAppend (Word α) (Word α) (Word α) := ⟨List.append⟩
-
-instance : SizeOf (Word α) := ⟨List.length⟩
-
-instance : Coe (Word α) (List α) := ⟨id⟩
-
-instance [DecidableEq α] : Coe (Word α) (Finset α) := ⟨fun w => w.toFinset⟩
-
-instance : Membership α (Word α) := ⟨List.Mem⟩
-
-instance : EmptyCollection (Word α) := ⟨[]⟩
-
-instance : Inhabited (Word α) := ⟨∅⟩
-
-end Word
+/-  Note: We distinguish `w` as a `Word` from `l` as a `List`, but use the same type
+    as there are so many functionalities given with `List`. -/
 
 /-- Language version of greedoid. -/
 structure GreedoidLanguage (α : Type _) [Fintype α] where
   /-- `language` is a finite sequence of words. -/
-  language : Finset (Word α)
+  language : Finset (List α)
   /-- Every words of the `language` are simple, i.e., no words contains duplicates. -/
-  simple : ∀ w ∈ language, w.simple
+  simple : ∀ w ∈ language, w.Nodup
   /-- `language` contains an empty word. -/
   contains_empty : ∅ ∈ language
   /-- For every word `w = w₂ ++ w₁ ∈ language`, `w₁ ∈ language` also holds. -/
-  contains_prefix : ∀ w₁ w₂ : Word α, w₂ ++ w₁ ∈ language → w₁ ∈ language
+  contains_prefix : ∀ w₁ w₂ : List α, w₂ ++ w₁ ∈ language → w₁ ∈ language
   /-- Exchange Axiom -/
-  exchange_axiom : {w₁ : Word α} → w₁ ∈ language → {w₂ : Word α} → w₂ ∈ language →
+  exchange_axiom : {w₁ : List α} → w₁ ∈ language → {w₂ : List α} → w₂ ∈ language →
     w₁.length > w₂.length → ∃ x ∈ w₁, x :: w₂ ∈ language
 
 /-- Set System version of greedoid. -/
@@ -61,6 +31,40 @@ structure GreedoidSystem (α : Type _) [Fintype α] [DecidableEq α] where
   exchange_axiom : {s₁ : Finset α} → s₁ ∈ feasible_set → {s₂ : Finset α} → s₂ ∈ feasible_set →
     s₁.card > s₂.card → ∃ x ∈ s₁ \ s₂, s₂ ∪ {x} ∈ feasible_set
 
+/-- Checks if the converted set equals the feasible set.
+
+    `feasible_set = {↑w | w ∈ language}` -/
+protected def Greedoid.fromLanguageToSystem {α : Type _} [Fintype α] [DecidableEq α]
+  (L : GreedoidLanguage α) := L.language.image (fun w : List α => (w.toFinset : Finset α))
+
+/-- Checks if the converted language equals the language.
+
+    `language = {w | underlying set of every prefix of w is feasible}` -/
+protected def Greedoid.fromSystemToLanguage {α : Type _} [Fintype α] [DecidableEq α]
+  (S : GreedoidSystem α) : Finset (List α) :=
+  let perm_set := Finset.bunionᵢ (S.feasible_set.map ⟨fun s => s.val.lists, by
+    intro s
+    have : ∃ l, l ∈ s.val.lists := by
+      induction' s using Finset.induction_on with a s hs ih
+      . exists []; simp
+      . let ⟨l, hl⟩ := ih
+        exists a :: l
+        have : a ∉ l := by
+          intro h'; apply hs; clear hs
+          simp [Multiset.mem_lists_iff] at hl
+          rw [Finset.mem_def, hl]
+          simp [h']
+        simp_all
+    intro t h
+    simp at h
+    let ⟨l, hl⟩ := this
+    have hr := h ▸ hl
+    have hl := (s.val.mem_lists_iff l).mp hl
+    have hr := (t.val.mem_lists_iff l).mp hr
+    apply Finset.eq_of_veq
+    simp only [hl, hr]⟩) id
+  (perm_set.filter (fun l => ∀ l' ∈ l.tails, l'.toFinset ∈ S.feasible_set))
+
 /-- `relatedLanguageSystem` checks if a given language and system are related to each other.
     That is, given that the language is hereditary,
 
@@ -68,8 +72,8 @@ structure GreedoidSystem (α : Type _) [Fintype α] [DecidableEq α] where
     2. `language = {w | underlying set of every prefix of w is feasible}` -/
 protected def Greedoid.relatedLanguageSystem {α : Type _} [Fintype α] [DecidableEq α]
   (L : GreedoidLanguage α) (S : GreedoidSystem α) : Prop :=
-  S.feasible_set = L.language.image (fun w : Word α => (↑w : Finset α)) ∧
-  sorry -- `language = {w | underlying set of every prefix of w is feasible}`
+  S.feasible_set = Greedoid.fromLanguageToSystem L ∧
+  L.language = Greedoid.fromSystemToLanguage S
 
 /-- Merging of language and system version of greedoid.
     This will (potentially) help `Greedoid` cover theorems written in
