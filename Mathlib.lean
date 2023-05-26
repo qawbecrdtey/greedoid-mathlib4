fd3c2a1b413dcd1e620fc8906a9e37076874e7ed
Jujian Zhang <jujian.zhang1998@outlook.com>
2023-05-26 11:51:45 +0000

    feat: port Topology.Sheaves.SheafCondition.PairwiseIntersections (#4384)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2473,6 +2473,7 @@ import Mathlib.Topology.Sheaves.Presheaf
 import Mathlib.Topology.Sheaves.PresheafOfFunctions
 import Mathlib.Topology.Sheaves.Sheaf
 import Mathlib.Topology.Sheaves.SheafCondition.OpensLeCover
+import Mathlib.Topology.Sheaves.SheafCondition.PairwiseIntersections
 import Mathlib.Topology.Sheaves.SheafCondition.Sites
 import Mathlib.Topology.ShrinkingLemma
 import Mathlib.Topology.Sober
