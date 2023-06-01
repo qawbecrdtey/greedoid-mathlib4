0ad9ac40da6b065d8ee4359b33cca4f45b5be9c3
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-06-01 10:18:42 +0000

    feat: port Topology.MetricSpace.Kuratowski (#4544)

--- Mathlib.lean
+++ Mathlib.lean
@@ -2527,6 +2527,7 @@ import Mathlib.Topology.MetricSpace.Holder
 import Mathlib.Topology.MetricSpace.Infsep
 import Mathlib.Topology.MetricSpace.IsometricSMul
 import Mathlib.Topology.MetricSpace.Isometry
+import Mathlib.Topology.MetricSpace.Kuratowski
 import Mathlib.Topology.MetricSpace.Lipschitz
 import Mathlib.Topology.MetricSpace.MetricSeparated
 import Mathlib.Topology.MetricSpace.Metrizable
