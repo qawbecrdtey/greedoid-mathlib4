126d92c700044d6a26bb56eea65bb44fd79b65c5
Yury G. Kudryashov <urkud@urkud.name>
2023-06-07 00:58:03 +0000

    feat: port Topology.Order.Hom.Esakia (#3108)
    Co-authored-by: int-y1 <jason_yuen2007@hotmail.com>
    Co-authored-by: Matthew Ballard <matt@mrb.email>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2720,6 +2720,7 @@ import Mathlib.Topology.OmegaCompletePartialOrder
 import Mathlib.Topology.Order
 import Mathlib.Topology.Order.Basic
 import Mathlib.Topology.Order.Hom.Basic
+import Mathlib.Topology.Order.Hom.Esakia
 import Mathlib.Topology.Order.Lattice
 import Mathlib.Topology.Order.LowerTopology
 import Mathlib.Topology.Order.Priestley
