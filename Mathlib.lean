733cf0d4781604fa25fe608d1b81d84b4e48f341
Moritz Firsching <moritz.firsching@gmail.com>
2023-06-08 04:21:50 +0000

    feat: port Topology.Category.Compactum (#3872)
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Matthew Ballard <matt@mrb.email>
    Co-authored-by: Johan Commelin <johan@commelin.net>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2656,6 +2656,7 @@ import Mathlib.Topology.Category.CompHaus.Basic
 import Mathlib.Topology.Category.CompHaus.EffectiveEpi
 import Mathlib.Topology.Category.CompHaus.ExplicitLimits
 import Mathlib.Topology.Category.CompHaus.Projective
+import Mathlib.Topology.Category.Compactum
 import Mathlib.Topology.Category.Locale
 import Mathlib.Topology.Category.Profinite.AsLimit
 import Mathlib.Topology.Category.Profinite.Basic
