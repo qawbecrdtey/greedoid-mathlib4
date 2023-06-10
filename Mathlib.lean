46995146ee9d7f1e0152f230379bb356f4d21279
Jeremy Tan Jie Rui <reddeloostw@gmail.com>
2023-06-10 05:46:50 +0000

    feat: port LinearAlgebra.Eigenspace.IsAlgClosed (#4943)

--- Mathlib.lean
+++ Mathlib.lean
@@ -1913,6 +1913,7 @@ import Mathlib.LinearAlgebra.DirectSum.Finsupp
 import Mathlib.LinearAlgebra.DirectSum.TensorProduct
 import Mathlib.LinearAlgebra.Dual
 import Mathlib.LinearAlgebra.Eigenspace.Basic
+import Mathlib.LinearAlgebra.Eigenspace.IsAlgClosed
 import Mathlib.LinearAlgebra.FiniteDimensional
 import Mathlib.LinearAlgebra.Finrank
 import Mathlib.LinearAlgebra.Finsupp
