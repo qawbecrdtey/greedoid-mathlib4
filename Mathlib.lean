3859ea6c35fbf25abf4f311d064db66d7cd271f3
Yury G. Kudryashov <urkud@urkud.name>
2023-07-06 09:30:16 +0000

    feat: define action of `Mᵈᵐᵃ` on `α →ₘ[μ] β` (#5693)
    See #5379

--- Mathlib.lean
+++ Mathlib.lean
@@ -2256,6 +2256,7 @@ import Mathlib.MeasureTheory.Decomposition.RadonNikodym
 import Mathlib.MeasureTheory.Decomposition.SignedHahn
 import Mathlib.MeasureTheory.Decomposition.UnsignedHahn
 import Mathlib.MeasureTheory.Function.AEEqFun
+import Mathlib.MeasureTheory.Function.AEEqFun.DomAct
 import Mathlib.MeasureTheory.Function.AEEqOfIntegral
 import Mathlib.MeasureTheory.Function.AEMeasurableOrder
 import Mathlib.MeasureTheory.Function.AEMeasurableSequence
