3a7fd861e9723391f3d1bd811c092c6813b03687
Gabin <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-07-07 17:05:24 +0000

    feat: port ModelTheory.Fraisse (#4565)
    Co-authored-by: Parcly Taxel <reddeloostw@gmail.com>
    Co-authored-by: Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
    Co-authored-by: Johan Commelin <johan@commelin.net>
    Co-authored-by: Matthew Ballard <matt@mrb.email>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2374,6 +2374,7 @@ import Mathlib.ModelTheory.DirectLimit
 import Mathlib.ModelTheory.ElementaryMaps
 import Mathlib.ModelTheory.Encoding
 import Mathlib.ModelTheory.FinitelyGenerated
+import Mathlib.ModelTheory.Fraisse
 import Mathlib.ModelTheory.Graph
 import Mathlib.ModelTheory.LanguageMap
 import Mathlib.ModelTheory.Order
